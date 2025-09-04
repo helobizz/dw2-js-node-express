b06e0c14e75372c9eb515b85ef0647d36379e618
DSM2 <fatec-dsm2@academico.intra>
2025-09-04 19:47:46 -0300

    exercício 4

---
+++ JavaScript/exercicios_js/ex003/ex004.js
@@ -0,0 +1,9 @@
+function escuro() {
+    document.getElementById("container").style.background = "black";
+    document.getElementById("container").style.color = "white";
+}
+
+function claro() {
+    document.getElementById("container").style.background = "white";
+    document.getElementById("container").style.color = "black";
+}
