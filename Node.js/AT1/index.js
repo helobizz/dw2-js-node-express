a82e1c7dbfe21477e258310053671f1a58825129
DSM2 <fatec-dsm2@academico.intra>
2025-09-25 20:34:37 -0300

    AT1

---
+++ Node.js/AT1/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+const port = 8080;
+
+app.listen(port, (error) => {
+  if (error) {
+    console.log(
+      `Não foi possível iniciar o servidor. Ocorreu um erro! ${error}`
+    );
+  } else {
+    console.log(`Servidor iniciado com sucesso em: http://localhost:${port}`);
+  }
+});
