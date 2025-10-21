12240c2b9a84fa40ce43a9cdef0f282f27225c86
DSM2 <fatec-dsm2@academico.intra>
2025-10-20 21:00:35 -0300

    CRUD Produtos!!

--- Node.js/nodejs-aula-05-SistemaLoja/controllers/ProdutosController.js
+++ Node.js/nodejs-aula-05-SistemaLoja/controllers/ProdutosController.js
@@ -6,11 +6,81 @@ import Produto from "../models/Produto.js";
 // ROTA PRODUTOS
 router.get("/produtos", function (req, res) {
   // SELECT * FROM PRODUTOS
-  Produto.findAll().then((produtos) => {
-    res.render("produtos", {
-      produtos: produtos,
+  Produto.findAll()
+    .then((produtos) => {
+      res.render("produtos", {
+        produtos: produtos,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+// ROTA DE CADASTRO DE PRODUTOS
+router.post("/produto/new", (req, res) => {
+  const nome = req.body.nome;
+  const preco = req.body.preco;
+  const categoria = req.body.categoria;
+  Produto.create({
+    nome: nome,
+    preco: preco,
+    categoria: categoria,
+  })
+    .then(() => {
+      res.redirect("/produtos");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+// ROTA DE EXCLUSÃO DE PRODUTO
+router.get("/produtos/delete/:id", (req, res) => {
+  const id = req.params.id;
+  Produto.destroy({
+    where: {
+      id: id,
+    },
+  })
+    .then(() => {
+      res.redirect("/produtos");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+// ROTA DE EDIÇÃO DE PRODUTO
+router.get("/produtos/edit/:id", (req, res) => {
+  const id = req.params.id;
+  Produto.findByPk(id).then((produto) => {
+    res.render("ProdutoEdit", {
+      produto: produto,
     });
   });
+});
+
+// ROTA DE ALTERAÇÃO DE PRODUTO
+router.post("/produtos/update", (req, res) => {
+  const id = req.body.id;
+  const nome = req.body.nome;
+  const preco = req.body.preco;
+  const categoria = req.body.categoria;
+  Produto.update(
+    {
+      nome: nome,
+      preco: preco,
+      categoria: categoria,
+    },
+    { where: { id: id } }
+  )
+    .then(() => {
+      res.redirect("/produtos");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 export default router;
